4443daaa04f53315a407a88adf2b51ab79d74b2b
Omerge123 <96895137+Omerge123@users.noreply.github.com>
2022-01-29 13:03:31 +0800

    Update Crypto_Autorun_Streamlit.py

--- Crypto_Autorun_Streamlit.py
+++ Crypto_Autorun_Streamlit.py
@@ -89,6 +89,18 @@ st.header('DeFi Llama Total Value Locked (TVL) Chain')
 # charts
 st.sidebar.markdown(today_run_date)
 select_event = st.sidebar.selectbox('Selection',options)
+
+st.markdown(f'''
+    <style>
+        section[data-testid="stSidebar"] .css-ng1t4o {{width: 14rem;}}
+        section[data-testid="stSidebar"] .css-1d391kg {{width: 14rem;}}
+    </style>
+''',unsafe_allow_html=True)
+
+
 text(select_event)
 chart(select_event)
 chart2(select_event)
+
+
+
